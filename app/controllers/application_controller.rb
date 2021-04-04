85c3a4e56b65fc687cd03cbe702ab28ce606b7fe
EC2 Default User <ec2-user@ip-172-31-15-10.ec2.internal>
2021-04-04 22:30:26 +0000

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except: [:top, :about]
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	#デバイス機能実行前にconfigure_permitted_parametersの実行をする。
+	protect_from_forgery with: :exception
+
+  protected
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit,:update]
+  
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+    
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    if @book.save #入力されたデータをdbに保存する。
+      redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+  
+  def update
+   @user = User.find(params[:id])
+    if @user.update(user_params)
+    #users_pathからuser_pathに変更
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  
+ private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+      unless @user == current_user
+        redirect_to user_path(current_user)
+      end
+  end
+  
+end
+
